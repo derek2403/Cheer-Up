5d0d123e4374386272b87581da35b8381324609b
Emungithub <eemunsm@gmail.com>
2025-04-12 12:04:05 +0800

    Add 3D model viewer with base.obj integration

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
-import "../styles/globals.css";
+import "@/styles/globals.css";
 import { HeroUIProvider } from "@heroui/react";
+import Header from '../components/Header';
 
 export default function App({ Component, pageProps }) {
   return (
     <HeroUIProvider>
-      <Component {...pageProps} />
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow">
+          <Component {...pageProps} />
+        </main>
+      </div>
     </HeroUIProvider>
   );
 }
